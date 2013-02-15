2e271e5280da43bd24ff666d90114f0aeb9c4d3c
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-15 15:50:33 -0600

    added google analytics

--- index.php
+++ index.php
@@ -238,6 +238,22 @@ if (isset($_POST['submit']) && isset($_GET['idea']) && $_GET['idea'] == 'submitt
 
 </div><!-- #page -->
 
+<?php if (!is_user_logged_in()) { ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38508639-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<?php } ?>
+
 <script src="<?php echo bloginfo('template_url'); ?>/js/plugins.js"></script>
 <script src="<?php echo bloginfo('template_url'); ?>/js/script.js"></script>
 
